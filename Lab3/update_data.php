aa04d34300ac04434ea3d041809a27be4750b434
sharmp5 <sharmp5@rpi.edu>
2024-10-16 20:54:14 -0400

    Updating update_data.php
    Will this finally be able to let users update nasa API?

--- Lab3/update_data.php
+++ Lab3/update_data.php
@@ -8,41 +8,28 @@ $user = 'root';
 $password = 'Pranay36951!!';
 $dbname = 'weatherastronomydb';
 
+// Connect to MySQL database
 $conn = new mysqli($host, $user, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Receive and decode JSON data
 $data = json_decode(file_get_contents('php://input'), true);
 
-if (isset($data['weather_data'])) {
-    $weatherData = json_decode($data['weather_data'], true);
-    
-    if (isset($weatherData['temperature'], $weatherData['description'], $weatherData['humidity'], $weatherData['wind_speed'])) {
-        $temperature = $conn->real_escape_string($weatherData['temperature']);
-        $description = $conn->real_escape_string($weatherData['description']);
-        $humidity = $conn->real_escape_string($weatherData['humidity']);
-        $wind_speed = $conn->real_escape_string($weatherData['wind_speed']);
-
-        $sql = "UPDATE weather_data SET temperature='$temperature', description='$description', humidity='$humidity', wind_speed='$wind_speed' ORDER BY id DESC LIMIT 1";
-        if ($conn->query($sql) === TRUE) {
-            echo "Weather data updated successfully!";
-        } else {
-            echo "Error updating weather data: " . $conn->error;
-        }
-    } else {
-        echo "Invalid weather data input.";
-    }
-} elseif (isset($data['astronomy_data'])) {
+// Handle the update based on the type of data
+if (isset($data['astronomy_data'])) {
     $astronomyData = json_decode($data['astronomy_data'], true);
-    
-    if (isset($astronomyData['title'], $astronomyData['explanation'], $astronomyData['image_url'])) {
+
+    // Check if all required fields are present
+    if (isset($astronomyData['title']) && isset($astronomyData['explanation']) && isset($astronomyData['image_url'])) {
         $title = $conn->real_escape_string($astronomyData['title']);
         $explanation = $conn->real_escape_string($astronomyData['explanation']);
         $image_url = $conn->real_escape_string($astronomyData['image_url']);
 
-        $sql = "UPDATE astronomy_data SET title='$title', explanation='$explanation', image_url='$image_url' ORDER BY id DESC LIMIT 1";
+        $sql = "UPDATE astronomy_data SET title='$title', explanation='$explanation', image_url='$image_url' WHERE id=1";
         if ($conn->query($sql) === TRUE) {
             echo "Astronomy data updated successfully!";
         } else {
@@ -50,10 +37,10 @@ if (isset($data['weather_data'])) {
         }
     } else {
         echo "Invalid astronomy data input.";
+        error_log("Astronomy data missing fields: " . print_r($astronomyData, true)); // Debugging log
     }
 } else {
     echo "Invalid data format.";
 }
-
 $conn->close();
 ?>
